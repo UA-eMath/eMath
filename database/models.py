f71e3e52af1f3b417ca43f35a67c2786c8910bd1
YAOZHI LU <luyaozhiusing@gmail.com>
2019-05-28 14:52:00 -0600

    5/28 basic structure done, need one link table

--- database/models.py
+++ database/models.py
@@ -2,24 +2,20 @@ from django.db import models
 
 # Create your models here.
 class Level(models.Model):
+
     '''
     This class represents a level structure.
 
         level_number : level number of unit
-
         position : position of this unit in corresponded level (units in each level should be ordered)
-
         next_level : point to next level table
-
         isPage : if all children under this node contains a page
-
         title : title of unit
-
         header : header of unit, could be null
-
         name : name of unit; e.g. Chapter, Section
 
     '''
+
     level_number = models.IntegerField()
     position =  models.IntegerField()
     next_level = models.ForeignKey(
@@ -31,37 +27,19 @@ class Level(models.Model):
     title = models.CharField(max_length = 100)
     header = models.CharField(max_length = 100,null = True)
 
-    para = models.ForeignKey(
-        'Para',
-        on_delete = models.CASCADE,
-    )
-
-    math_display = models.ForeignKey(
-        'MathDisplay',
-        on_delete = models.CASCADE,
-    )
-
-    mini_Compo = models.ForeignKey(
-        'MiniCompositor',
-        on_delete = models.CASCADE,
-
-    )
-
     class meta:
         abstract = True
         ordering = ('position')
 
 
 class Book(Level):
+
     '''
     This class represents a book structure extend level class.
 
         html_title : Version of title for the browser title bar
-
         authors : List of all authors of the book
-
         contributors : List of all contributors to the book; e.g. programmers, graphic designers, etc.
-
         date : Date
     '''
 
@@ -83,33 +61,34 @@ class Para(models.Model):
     '''
     This class represents a paragraph structure.
 
-            content :
-            external_link
-            position
+            content : text of paragraph
+            external_link : an external url
+            position : position of this paragraph in upper structure
             para_type : format of paragraph; e.g. Text, Table(in Json), List(in Json)
-            caption
-            internal_link
+            caption : caption of this paragraph
+            internal_link : #
 
     '''
 
     content = models.TextField()
-    external_link = models.ManyToManyField('Link')
     position = models.IntegerField()
     para_type = models.CharField(max_length= 15)
     caption = models.CharField(max_length = 100, null=True)
-    internal_link = models.ForeignKey(
-        Level,
-        on_delete = models.CASCADE
-    )
+
+    upper_level = models.ForeignKey(Level, on_delete=models.CASCADE)
+    upper_mini_compositor = models.ForeignKey('MiniCompositor', on_delete=models.CASCADE)
 
     class Meta:
         ordering = ("position")
 
 
 class MathDisplay(models.Model):
-    '''
-    This class represents a
 
+    '''
+
+    This class represents a math.display structure(latex)
+        position :  position of math display in upper structure
+        content : latex in string
 
     '''
 
@@ -122,34 +101,33 @@ class MathDisplay(models.Model):
 
 
 class Link(models.Model):
-    '''
-    This class represents a
 
+    '''
+
+    This class represents a Link structure
+
+        url : url
 
     '''
 
     url = models.URLField(max_length = 200, null = True)
 
+    external_link = models.ManyToManyField('Link')
 
 
 class MiniCompositor(models.Model):
-    '''
-    This class represents a
 
+    '''
+    This class represents a mini compositor structure.
+
+        category : category of mini compositor. e.g. example, proof, def, theorem
+        position : position of mini compositor in upper structure
 
     '''
 
     category = models.CharField(max_length=30)
     position = models.IntegerField()
-    para = models.ForeignKey(
-        Para,
-        on_delete = models.CASCADE,
-    )
 
-    math_display = models.ForeignKey(
-        MathDisplay,
-        on_delete = models.CASCADE,
-    )
 
     class Meta:
         ordering = ("position")
