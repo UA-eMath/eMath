35239528c962c6ff6409e10cc93d6d45e55883e3
YAOZHI LU <luyaozhiusing@gmail.com>
2019-06-07 14:02:37 -0600

    6/7 split view partially done

--- frontend/src/layouts/SplitView/index.js
+++ frontend/src/layouts/SplitView/index.js
@@ -1,25 +1,96 @@
 import React from 'react'
 import 'antd/dist/antd.css';
-import { Row, Col } from 'antd';
-import { Layout } from 'antd';
-import SplitPane from 'react-split-pane'
+
+import _ from "lodash";
+import { Responsive, WidthProvider } from "react-grid-layout";
+const ResponsiveReactGridLayout = WidthProvider(Responsive);
+
+
 export default class SplitView extends React.Component{
+      static defaultProps = {
+    className: "layout",
+    rowHeight: 30,
+    onLayoutChange: function() {},
+    cols: { lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 },
+    initialLayout: generateLayout()
+  };
+
+  state = {
+    currentBreakpoint: "lg",
+    compactType: "horizontal",
+    mounted: false,
+    layouts: { lg: this.props.initialLayout }
+  };
+
+  componentDidMount() {
+    this.setState({ mounted: true });
+  }
+
+  generateDOM() {
+    return _.map(this.state.layouts.lg, function(l, i) {
+      return (
+        <div key={i} className={l.static ? "static" : ""}>
+          {l.static ? (
+            <span
+              className="text"
+              title="This item is static and cannot be removed or resized."
+            >
+              Static - {i}
+            </span>
+          ) : (
+            <span className="text">{i}</span>
+          )}
+        </div>
+      );
+    });
+  }
+
+  onBreakpointChange = breakpoint => {
+    this.setState({
+      currentBreakpoint: breakpoint
+    });
+  };
+
+
+  onLayoutChange = (layout, layouts) => {
+    this.props.onLayoutChange(layout, layouts);
+  };
 
 
 
-    render(){
-        const {  Content } = Layout;
+  render() {
+    return (
+      <div>
 
-        return(
-            <SplitPane split="vertical" minSize={50} defaultSize={100}>
-                 <Content style={{ margin: '2em 2em 0' }}>
-                    <div style={{ padding: 24, background: '#fff', minHeight: 1000 }}>content</div>
-                 </Content>
-                <Content style={{ margin: '2em 2em 0' }}>
-                    <div style={{ padding: 24, background: '#fff', minHeight: 1000 }}>content</div>
-                </Content>
-            </SplitPane>
-            
-        )
-    }
+        <ResponsiveReactGridLayout
+          {...this.props}
+          layouts={this.state.layouts}
+          onBreakpointChange={this.onBreakpointChange}
+          onLayoutChange={this.onLayoutChange}
+          // WidthProvider option
+          measureBeforeMount={false}
+          // I like to have it animate on mount. If you don't, delete `useCSSTransforms` (it's default `true`)
+          // and set `measureBeforeMount={true}`.
+          useCSSTransforms={this.state.mounted}
+          compactType={this.state.compactType}
+          preventCollision={!this.state.compactType}
+        >
+          {this.generateDOM()}
+        </ResponsiveReactGridLayout>
+      </div>
+    );
+  }
+}
+function generateLayout() {
+  return _.map(_.range(0, 2), function(item, i) {
+    let y = Math.ceil(Math.random() * 4) + 1;
+    return {
+      x: (_.random(0, 5) * 2) % 12,
+      y: Math.floor(i / 6) * y,
+      w: 2,
+      h: y,
+      i: i.toString(),
+      static: Math.random() < 0.05
+    };
+  });
 }
