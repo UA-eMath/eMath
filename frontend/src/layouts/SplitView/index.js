89cffcc8f3c9e7f7925c1e8b10b59ca26c4443fc
YAOZHI LU <luyaozhiusing@gmail.com>
2019-06-07 13:40:53 -0600

    6/7 Found a FANTANSTIC split view library

--- frontend/src/layouts/SplitView/index.js
+++ frontend/src/layouts/SplitView/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import 'antd/dist/antd.css';
 import { Row, Col } from 'antd';
 import { Layout } from 'antd';
-
+import SplitPane from 'react-split-pane'
 export default class SplitView extends React.Component{
 
 
@@ -11,24 +11,15 @@ export default class SplitView extends React.Component{
         const {  Content } = Layout;
 
         return(
-            <Row gutter={16}>
-                <Col span={12}>
-                    <Layout>
-                        <Content style={{ margin: '2em 2em 0' }}>
-                            <div style={{ padding: 24, background: '#fff', minHeight: 1000 }}>content</div>
-                        </Content>
-                    </Layout>
-                </Col>
-
-                <Col span={12}>
-                    <Layout>
-                        <Content style={{ margin: '2em 2em 0' }}>
-                            <div style={{ padding: 24, background: '#fff', minHeight: 1000 }}>content</div>
-                        </Content>
-                    </Layout>
-                </Col>
-            </Row>
-
+            <SplitPane split="vertical" minSize={50} defaultSize={100}>
+                 <Content style={{ margin: '2em 2em 0' }}>
+                    <div style={{ padding: 24, background: '#fff', minHeight: 1000 }}>content</div>
+                 </Content>
+                <Content style={{ margin: '2em 2em 0' }}>
+                    <div style={{ padding: 24, background: '#fff', minHeight: 1000 }}>content</div>
+                </Content>
+            </SplitPane>
+            
         )
     }
 }
