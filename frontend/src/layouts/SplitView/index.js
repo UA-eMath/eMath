98c768e480233e52ac532cca3225da72b482eca2
YAOZHI LU <luyaozhiusing@gmail.com>
2019-06-08 22:32:13 -0600

    6/8 edit split view, now it can add/remove a block

--- frontend/src/layouts/SplitView/index.js
+++ frontend/src/layouts/SplitView/index.js
@@ -12,84 +12,115 @@ const ResponsiveReactGridLayout = WidthProvider(Responsive);
 
 
 export default class SplitView extends React.Component{
-   static defaultProps = {
+  static defaultProps = {
     className: "layout",
-    rowHeight: 30,
-    onLayoutChange: function() {},
     cols: { lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 },
-    initialLayout: generateLayout()
+    rowHeight: 100,
+      verticalCompact:false
   };
 
-  state = {
-    currentBreakpoint: "lg",
-    compactType: "horizontal",
-    mounted: false,
-    layouts: { lg: this.props.initialLayout }
-  };
+  constructor(props) {
+    super(props);
 
-  componentDidMount() {
-    this.setState({ mounted: true });
+    this.state = {
+      items: [0, 1].map(function(i, key, list) {
+        return {
+          i: i.toString(),
+          x: i * 6,
+          y: 0,
+          w: 6,
+          h: 12,
+          add: i === (list.length - 1).toString()
+        };
+      }),
+      newCounter: 0,
+    };
+
+    this.onAddItem = this.onAddItem.bind(this);
+    this.onBreakpointChange = this.onBreakpointChange.bind(this);
   }
 
-  generateDOM() {
-    return _.map(this.state.layouts.lg, function(l, i) {
-      return (
-        <div key={i} style={ {background:'grey'}}>
-            <Scrollbars>
-                <span className="text">{i}</span>
-            </Scrollbars>
-        </div>
-      );
-    });
-  }
-
-  onBreakpointChange = breakpoint => {
-    this.setState({
-      currentBreakpoint: breakpoint
-    });
-  };
-
-
-
-  onLayoutChange = (layout, layouts) => {
-    this.props.onLayoutChange(layout, layouts);
-  };
-
-
-
-  render() {
+  createElement(el) {
+    const removeStyle = {
+      position: "absolute",
+      right: "2px",
+      top: 0,
+      cursor: "pointer"
+    };
+    const i = el.add ? "+" : el.i;
     return (
-      <div>
-
-        <ResponsiveReactGridLayout
-          {...this.props}
-          layouts={this.state.layouts}
-          onBreakpointChange={this.onBreakpointChange}
-          onLayoutChange={this.onLayoutChange}
-          // WidthProvider option
-          measureBeforeMount={false}
-          // I like to have it animate on mount. If you don't, delete `useCSSTransforms` (it's default `true`)
-          // and set `measureBeforeMount={true}`.
-          useCSSTransforms={this.state.mounted}
-          compactType={this.state.compactType}
-          preventCollision={!this.state.compactType}
+      <div key={i} data-grid={el}
+              style={{background:'LightBlue'}}>
+        {el.add ? (
+          <span
+            className="add text"
+            onClick={this.onAddItem}
+            title="You can add an item by clicking here, too."
+          >
+            Add +
+          </span>
+        ) : (
+          <span className="text">{i}</span>
+        )}
+        <span
+          className="remove"
+          style={removeStyle}
+          onClick={this.onRemoveItem.bind(this, i)}
         >
-          {this.generateDOM()}
-        </ResponsiveReactGridLayout>
+          x
+        </span>
       </div>
     );
   }
 
-}
-function generateLayout() {
-  return _.map(_.range(0, 2), function(item, i) {
-    let y = Math.ceil(Math.random() * 4) + 1;
-    return {
-      x: (_.random(0, 5) * 2) % 12,
-      y: Math.floor(i / 6) * y,
-      w: 2,
-      h: y,
-      i: i.toString(),
-    };
-  });
+  onAddItem() {
+    /*eslint no-console: 0*/
+    console.log("adding", "n" + this.state.newCounter);
+    this.setState({
+      // Add a new item. It must have a unique key!
+      items: this.state.items.concat({
+        i: "n" + this.state.newCounter,
+        x: (this.state.items.length * 2) % (this.state.cols || 12),
+        y: Infinity, // puts it at the bottom
+        w: 2,
+        h: 2
+      }),
+      // Increment the counter to ensure key is always unique.
+      newCounter: this.state.newCounter + 1
+    });
+  }
+
+  // We're using the cols coming back from this to calculate where to add new items.
+  onBreakpointChange(breakpoint, cols) {
+    this.setState({
+      breakpoint: breakpoint,
+      cols: cols
+    });
+  }
+
+  onLayoutChange(layout) {
+    this.props.onLayoutChange(layout);
+    this.setState({ layout: layout });
+  }
+
+  onRemoveItem(i) {
+    console.log("removing", i);
+    this.setState({ items: _.reject(this.state.items, { i: i }) });
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.onAddItem}>Add Item</button>
+        <ResponsiveReactGridLayout
+            onLayoutChange={() => this.onLayoutChange}
+            onBreakpointChange={() => this.onBreakpointChange}
+          {...this.props}
+
+        >
+          {_.map(this.state.items, el => this.createElement(el))}
+        </ResponsiveReactGridLayout>
+      </div>
+    );
+  }
 }
