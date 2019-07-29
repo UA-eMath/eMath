01b9546f585f5b046dcf7c872702958edb9b727d
YAOZHI LU <luyaozhiusing@gmail.com>
2019-07-29 15:31:01 -0600

    7/29 get id attribute from tag

--- frontend/src/components/SplitView/pageCreator/pageRenderer/index.js
+++ frontend/src/components/SplitView/pageCreator/pageRenderer/index.js
@@ -9,26 +9,28 @@ export default function contentProcessor(paraText, props) {
 
 function linkMarker(paraText, props) {
 
-	let linkExtractor = new RegExp("<iLink.*?>(.*?)<\/iLink>", "g");
+	let linkExtractor = new RegExp("<iLink.*?>(.*?)</iLink>", "g");
 	let idExtractor = new RegExp("id = '(.*?)'", "g");
 
 	return (_.map(paraText, (para) => {
 		//mark link
-		let parts = para;
-		parts = parts.content.split(linkExtractor);
-		for (let i = 1; i < parts.length; i += 2) {
-			//get Link to id
-			let id = parts[i].match(idExtractor);
-			if (id) {
-				id = id.map((val) => {
-					val.replace("id = ", "")
-				});
-			}
+		let parts_with_id = para.content.match(linkExtractor);
+		let parts = para.content.split(linkExtractor);
 
-			id = parseInt(id);
+		//index to loop id array
+		let j = 0;
+
+		for (let i = 1; i < parts.length; i += 2) {
+			//get Link_to id
+			let id = parts_with_id[j].split(idExtractor)[1];
+
+			id = Number(id);
+			console.log(id);
+
 			parts[i] = <a onClick={props.onWindowOpen} style={{color: 'red'}}>{parts[i]}</a>
+
+			j++;
 		}
-		console.log(parts);
 
 		return parts
 	}))
