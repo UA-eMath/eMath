c5ca29e04df531b2478df38e047c768d2080d672
YAOZHI LU <luyaozhiusing@gmail.com>
2019-08-16 11:06:17 -0600

    8/16 refactor code

--- frontend/src/components/SplitView/pageCreator/pageRenderer/index.js
+++ frontend/src/components/SplitView/pageCreator/pageRenderer/index.js
@@ -1,124 +1,21 @@
 import _ from "lodash";
 import React from 'react'
-import MathJax from 'react-mathjax'
+import * as utils from './utils'
 
 export default function contentProcessor(paraText, props) {
 	return (_.map(paraText, (para) => {
 
 		let processingPara = para.content;
 
-		processingPara = tagParser(processingPara, props);
-		// processingPara = linkMarker(processingPara, props);
-		// processingPara = mathDisplay(processingPara);
-		//
+		processingPara = utils.tagParser(processingPara, props);
+		processingPara = utils.addCaption(processingPara, para.caption);
 
-
-		processingPara = addCaption(processingPara, para.caption);
 
 		return (
-			<div>{processingPara}</div>
+			<div>
+				{processingPara}
+			</div>
 		)
 	}));
 
-}
-
-// give para caption here; style para here;
-function addCaption(para, caption) {
-
-	if (caption !== "null" && caption !== '') {
-		para.unshift(<span style={{fontWeight: "bold"}}>{caption+ '  '}</span>);
-	}
-
-	return para
-}
-
-//tag processor
-function tagParser(para, props) {
-
-	// regexs to match tags in string
-	let regex = {
-		link: {
-			phrase: new RegExp("(<iLink.*?>.*?</iLink>)", "g"),
-			id: new RegExp("id = '(.*?)'", "g"),
-			content: new RegExp("<iLink.*?>(.*?)</iLink>", "g")
-		},
-		latex: {
-			phrase: new RegExp("(<MathDisplay.*?>.*?</MathDisplay>)", "g"),
-			inline: new RegExp("inline = '(.*?)'", 'g'),
-			content: new RegExp("<MathDisplay.*?>(.*?)</MathDisplay>", "g"),
-		}
-	};
-
-	let paragraphs = para.split(regex.link.phrase) || [];
-
-
-	paragraphs = paragraphs.map((text, i) => {
-		let id = (i % 2 !== 0) ? (text.split(regex.link.id)[i]) : null;
-
-		if (id !== null) {
-
-			text = text.split(regex.link.content)[1];
-
-			return (
-				<a onClick={() => props.onWindowOpen(id)} style={{color: 'red'}} key={i}>
-					{
-						mathDisplay(text, regex)
-					}
-				</a>
-			)
-		} else {
-			return mathDisplay(text, regex)
-		}
-	});
-
-	return paragraphs
-}
-
-//process latex
-function mathDisplay(text, regex) {
-	//TODO: inline math or block math
-
-	const MathJaxConfig = {
-		script:
-			'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-MML-AM_CHTML',
-		options: {
-
-			extensions: ["tex2jax.js"],
-			jax: ["input/TeX", "output/HTML-CSS"],
-			"HTML-CSS": {
-				styles: {".MathJax_Preview": {visibility: "hidden"}}
-			},
-			"SVG": {
-				styles: {".MathJax_Preview": {visibility: "hidden"}}
-			},
-			tex2jax: {inlineMath: [["$", "$"], ["\\(", "\\)"]]},
-			TeX: {extensions: ["http://sonoisa.github.io/xyjax_ext/xypic.js", "AMSmath.js", "AMSsymbols.js"]}
-		}
-	};
-
-	let mathPart = text.split(regex.latex.phrase);
-
-
-	return mathPart.map((mathText, j) => {
-		if (j % 2 !== 1) {
-			return mathText
-		}
-		let inline = mathText.match(regex.latex.inline);
-
-		if (inline !== null) {
-			return (
-				<MathJax.Provider {...MathJaxConfig}>
-					<MathJax.Node inline formula={mathText.split(regex.latex.content)[1]}/>
-				</MathJax.Provider>)
-		} else {
-			//["inline = 'true'"]
-			return (
-				<MathJax.Provider {...MathJaxConfig}>
-					<div>
-						<MathJax.Node formula={mathText.split(regex.latex.content)[1]}/>
-					</div>
-				</MathJax.Provider>
-			)
-		}
-	})
 }
---
+++ frontend/src/components/SplitView/pageCreator/pageRenderer/utils/index.js
@@ -0,0 +1,105 @@
+import MathJax from 'react-mathjax'
+import React from 'react'
+
+// give para caption here; style para here;
+export function addCaption(para, caption) {
+
+	if (caption !== "null" && caption !== '') {
+		para.unshift(<span style={{fontWeight: "bold",paddingRight:"1.00em"}}>{caption}</span>);
+	}
+
+	return para
+}
+
+//tag processor
+export function tagParser(para, props) {
+
+	// regexs to match tags in string
+	let regex = {
+		link: {
+			phrase: new RegExp("(<iLink.*?>.*?</iLink>)", "g"),
+			id: new RegExp("id = '(.*?)'", "g"),
+			content: new RegExp("<iLink.*?>(.*?)</iLink>", "g")
+		},
+		latex: {
+			phrase: new RegExp("(<MathDisplay.*?>.*?</MathDisplay>)", "g"),
+			inline: new RegExp("inline = '(.*?)'", 'g'),
+			content: new RegExp("<MathDisplay.*?>(.*?)</MathDisplay>", "g"),
+		}
+	};
+
+	let paragraphs = para.split(regex.link.phrase) || [];
+
+
+	paragraphs = paragraphs.map((text, i) => {
+		let id = (i % 2 !== 0) ? (text.split(regex.link.id)[i]) : null;
+
+		if (id !== null) {
+
+			text = text.split(regex.link.content)[1];
+
+			return (
+				<a onClick={() => props.onWindowOpen(id)} style={{color: 'red'}} key={i}>
+					{
+						mathDisplay(text, regex)
+					}
+				</a>
+			)
+		} else {
+			return mathDisplay(text, regex)
+		}
+	});
+
+	return paragraphs
+}
+
+//process latex
+function mathDisplay(text, regex) {
+	//TODO: inline math or block math
+
+	const MathJaxConfig = {
+		script:
+			'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-MML-AM_CHTML',
+		options: {
+
+			extensions: ["tex2jax.js"],
+			jax: ["input/TeX", "output/HTML-CSS"],
+			"HTML-CSS": {
+				styles: {".MathJax_Preview": {visibility: "hidden"}}
+			},
+			"SVG": {
+				styles: {".MathJax_Preview": {visibility: "hidden"}}
+			},
+			tex2jax: {inlineMath: [["$", "$"], ["\\(", "\\)"]]},
+			TeX: {extensions: ["http://sonoisa.github.io/xyjax_ext/xypic.js", "AMSmath.js", "AMSsymbols.js"]}
+		}
+	};
+
+	let mathPart = text.split(regex.latex.phrase);
+
+
+	return mathPart.map((mathText, j) => {
+		if (j % 2 !== 1) {
+			return mathText
+		}
+		let inline = mathText.match(regex.latex.inline);
+
+		if (inline !== null) {
+			return (
+				<MathJax.Provider {...MathJaxConfig}>
+					<MathJax.Node inline formula={mathText.split(regex.latex.content)[1]}/>
+				</MathJax.Provider>)
+		} else {
+			//["inline = 'true'"]
+			return (
+				<MathJax.Provider {...MathJaxConfig}>
+					<div>
+						<MathJax.Node formula={mathText.split(regex.latex.content)[1]}/>
+					</div>
+				</MathJax.Provider>
+			)
+		}
+	})
+}
+
+
