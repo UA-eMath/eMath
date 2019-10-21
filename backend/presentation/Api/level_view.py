e47b272893b29c8808ef02fdf2a34ab78c5c62f6
YAOZHI LU <luyaozhiusing@gmail.com>
2019-10-21 16:26:56 -0600

    10/21 finished level append/insertion with position and page number automated filling

--- backend/presentation/Api/level_view.py
+++ backend/presentation/Api/level_view.py
@@ -21,36 +21,56 @@ class LevelViewset(viewsets.ModelViewSet):
 
 
 	def create(self, request, *args, **kwargs):
-
 		request_data = request.data.copy()
 		# automated add position and pageNum, default increase one level under parent node
-		parent_position = Level.objects.get(pk=request_data['parent']).position
-		if not request_data['pageNum'] and request_data['isPage'] and parent_position != -1:
-			# get new pageNum
-			new_page_num = Level.objects.filter(isPage=True).order_by('-pageNum').first()
-			if new_page_num == None:
-				new_page_num = 1
-			else:
-				new_page_num = new_page_num.pageNum + 1
 
-			request_data['pageNum'] = new_page_num
-
+		#append
+		# get last position
+		children_list = Level.objects.get(id=request_data['parent']).get_children().order_by('position')
+		last_level = children_list.last()
 		if not request_data['position']:
-			# get new position
-			new_position = Level.objects.get(id=request_data['parent']).get_children().order_by('-position').first()
-			if new_position == None:
+			if last_level == None:
 				new_position = 0
 			else:
-				new_position = new_position.position + 1
+				new_position = last_level.position + 1
 
 			request_data['position'] = new_position
 
+		#insert
+		#Update children position when insert between nodes.
+		#TODO Update sub level position
+		elif int(request_data['position']) <= last_level.position:
+			cached_list = children_list[int(request_data['position']):]
+			for child in cached_list:
+				child.position += 1
+				child.save()
+
 
 		serializer = self.serializer_class(data=request_data)
-
 		serializer.is_valid(raise_exception=True)
 		serializer.save()
+
+		self._updatePageNumber(Level.objects.get(id=request_data['parent']).get_root())
+
 		return Response(serializer.data, 200)
+
+	def _updatePageNumber(self,root):
+		page_number = 1
+		def _recursivelyUpdatePageNum(root,page_number):
+			# base
+			if root.isPage:
+				root.pageNum = page_number
+				root.save()
+				return page_number + 1
+
+			if root.position >=0:
+				for child in root.get_children().order_by("position"):
+					page_number = _recursivelyUpdatePageNum(child,page_number)
+
+			return page_number
+		_recursivelyUpdatePageNum(root, page_number)
+		return
+
 
 	def destroy(self, request,pk=None):
 		try:
--- backend/presentation/admin.py
+++ backend/presentation/admin.py
@@ -1,14 +1,12 @@
 from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
 
 # Register your models here.
 from .models import *
     #Level, Book, Person, Para, MiniCompositor, MathDisplay, Link
 
-admin.site.register(Level)
+admin.site.register(Level,MPTTModelAdmin)
 admin.site.register(Person)
 admin.site.register(Para)
 admin.site.register(ExternalLink)
-
-
-
 
