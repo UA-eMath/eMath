30d2c55491834585900dec0497c1ec446e6f73fa
YAOZHI LU <luyaozhiusing@gmail.com>
2019-10-23 14:24:05 -0600

    10/23 finished block of content title

--- frontend/src/components/splitView/pageCreator/pageRenderer/index.js
+++ frontend/src/components/splitView/pageCreator/pageRenderer/index.js
@@ -4,6 +4,8 @@ import * as utils from './utils'
 import {addCaption} from "./utils/caption";
 import {listProcessor} from "./utils/list";
 import {tableProcessor} from "./utils/table";
+import {tagParser} from "./utils";
+import {PageHeader} from 'antd';
 
 /*
 A Para JSON structure is following:
@@ -19,8 +21,11 @@ A Para JSON structure is following:
 export default function contentProcessor(paraText, props) {
 
 	return (_.map(paraText, para => {
-		if(Array.isArray(para)){
-			return blockOfPara(para);
+		if (Array.isArray(para)) {
+			let left_title = para[0].para_parent.tocTitle;
+			let right_title = utils.tagParser(para[0].para_parent.title, props);
+
+			return blockOfPara(para, left_title, right_title);
 		}
 
 		let processingPara = para.content;
@@ -36,16 +41,16 @@ export default function contentProcessor(paraText, props) {
 		}
 
 		else if (type === "table") {
-			table = tableProcessor(data,props)
+			table = tableProcessor(data, props)
 		}
 
 		else if (type === "list") {
-			list = listProcessor(data,props);
+			list = listProcessor(data, props);
 		}
 
 		return (
 			<div key={_.uniqueId("div_")}>
-				<div style={{"textAlign":textAlign,"marginBottom":"10px"}}>
+				<div style={{"textAlign": textAlign, "marginBottom": "10px"}}>
 					{text}
 				</div>
 
@@ -60,18 +65,45 @@ export default function contentProcessor(paraText, props) {
 //  A block of paras which are inside of a sub-level will be represented as an inner array.
 // For a para array, inner para will be like: [xxx,[xxx,xxx],xxx]
 
-function blockOfPara(dataArray) {
+function blockOfPara(dataArray, left_title, right_title) {
+	console.log(dataArray);
+	let boxHeader;
 
-	return(
+	if (left_title || right_title) {
+		boxHeader =
+			<div
+				style={{
+					background: 'linear-gradient(#fdf5e8,#EAE7DC)',
+					borderRadius: '2px 2px 0 0',
+					padding: "2px 4px 2px 4px"
+				}}>
+				<span>
+					<b>{left_title}</b>
+				</span>
+				<span
+					style={{float: "right"}}>
+					<b>{right_title}</b>
+				</span>
+			</div>
+	}
+
+	return (
 		<div
 			key={_.uniqueId("blockOfPara_")}
-			style={{background: '#fdf5e8',
-                    borderRadius: '2px',
-				    boxShadow: '0 0 0 1px rgba(0,0,0,0.1), 0 1px 2px rgba(0,0,0,0.35)',
-				    margin: '1em .9em',
-				    padding: '.25em 1.25em .1em'
+			style={{
+				background: '#fdf5e8',
+				borderRadius: '2px',
+				boxShadow: '0 0 0 1px rgba(0,0,0,0.1), 0 1px 2px rgba(0,0,0,0.35)',
 			}}>
-			{contentProcessor(dataArray)}
+			{boxHeader}
+			<div
+				style={{
+					padding: "2px 4px 2px 4px"
+				}}>
+				{contentProcessor(dataArray)}
+			</div>
+
 		</div>
+
 	)
 }
