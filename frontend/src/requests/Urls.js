b26db14c77732819f5151a10cb83464b357f356b
YAOZHI LU <luyaozhiusing@gmail.com>
2019-11-06 15:21:44 -0700

    11/6 deploy

--- frontend/src/requests/Urls.js
+++ frontend/src/requests/Urls.js
@@ -1,10 +1,10 @@
 
-const local = true;
-const domain = local ? "http://localhost" : "somewhere.backend";
+const local = false;
+const domain = local ? "http://localhost" : "142.244.163.57";
 
 export default {
     domain: domain,
-    port: 8000,
+    port: local ? 8000 : 9000,
     endpoints: {
     	roots:{
     		address:'database',
