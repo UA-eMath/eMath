12c2921fab159bd7014b423f2baa7a13d839c09f
xuechun <noglues666@gmail.com>
2021-01-14 11:25:19 -0500

    change MathJax config file

--- frontend/src/constants/MathJax_config.js
+++ frontend/src/constants/MathJax_config.js
@@ -1,18 +1,31 @@
 const MathJaxConfig = {
-	script:
-		'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-MML-AM_CHTML',
-	options: {
-		extensions: ["tex2jax.js"],
-		jax: ["input/TeX", "output/HTML-CSS"],
-		"HTML-CSS": {
-			styles: {".MathJax_Preview": {visibility: "hidden"}}
-		},
-		"SVG": {
-			styles: {".MathJax_Preview": {visibility: "hidden"}}
-		},
-		tex2jax: {inlineMath: [["$", "$"], ["\\(", "\\)"]]},
-		TeX: {extensions: ["[Extra]/annotations.js", "[Extra]/xypic.js", "AMSmath.js", "AMSsymbols.js","action.js"]}
-	}
+  script:
+    "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-AMS-MML_HTMLorMML", // Mathjax Combined Configurations
+  options: {
+    extensions: ["tex2jax.js"],
+    jax: ["input/TeX", "input/MathML", "output/HTML-CSS", "output/PreviewHTML"],
+    tex2jax: {
+      inlineMath: [
+        ["$", "$"],
+        ["\\(", "\\)"],
+      ],
+    },
+    TeX: {
+      extensions: [
+        "http://sonoisa.github.io/xyjax_ext/xypic.js",
+        "AMSmath.js",
+        "AMSsymbols.js",
+        "action.js",
+      ],
+    },
+  },
 };
 
-export default MathJaxConfig
+export default MathJaxConfig;
+
+// "HTML-CSS": {
+//   styles: { ".MathJax_Preview": { visibility: "hidden" } },
+// },
+// SVG: {
+//   styles: { ".MathJax_Preview": { visibility: "hidden" } },
+// },
