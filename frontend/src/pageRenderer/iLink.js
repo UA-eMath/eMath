365d875b4c980b93bc2cf9c5a0ced776fb739872
xuechun <noglues666@gmail.com>
2021-03-02 11:55:15 -0500

    prevent crash when not found label in iLink

--- frontend/src/pageRenderer/iLink.js
+++ frontend/src/pageRenderer/iLink.js
@@ -3,6 +3,7 @@ import _ from "lodash";
 import { openNewWindow } from "../actions";
 import { connect } from "react-redux";
 import getLabel from "../requests/getLabel";
+import { message } from "antd";
 
 const mapDispatchToProps = (dispatch) => ({
   onWindowOpen: (pageId, isPage) => dispatch(openNewWindow(pageId, isPage)),
@@ -17,13 +18,19 @@ function iLink(props) {
       onClick={async () => {
         // get label using label id
         const linked_obj = await getLabel({ labelID: props.id });
-        props.onWindowOpen(
-          {
-            linkedID: linked_obj.data.linkedID,
-            linkTo: linked_obj.data.linkTo,
-          },
-          linked_obj.data.isPage
-        );
+        if (linked_obj === undefined) {
+          message.error(
+            "Fail to get cross reference! Please check your linked id!"
+          );
+        } else {
+          props.onWindowOpen(
+            {
+              linkedID: linked_obj.data.linkedID,
+              linkTo: linked_obj.data.linkTo,
+            },
+            linked_obj.data.isPage
+          );
+        }
       }}
       style={{ color: "#297DB5" }}
       key={_.uniqueId("iLink")}
