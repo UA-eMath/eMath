cf59bdb6618aa85cd92b9649001df8243d076ca0
xuechun <noglues666@gmail.com>
2021-05-29 23:11:34 -0400

    Adjust logic in open new window

--- frontend/src/reducers/windows.js
+++ frontend/src/reducers/windows.js
@@ -11,6 +11,7 @@ const initState_windows = {
       h: 9,
       add: i === (list.length - 1).toString(),
       static: true,
+      pageId: {},
     };
   }),
   newCounter: 0,
@@ -22,27 +23,22 @@ const windows = (state = initState_windows, action) => {
       if (typeof action.pageId !== "undefined" && action.pageId !== null) {
         // Only add page if not opened before
         // TODO: handle init window
-        const [, ...rest] = state.items;
-        const isExist = _.find(rest, function (e) {
+        const removeExisted = _.reject(state.items, function (e) {
           return e.pageId.id === action.pageId.id;
         });
-        if (isExist === undefined) {
-          return Object.assign({}, state, {
-            items: state.items.concat({
-              i: "n" + state.newCounter,
-              x: 6,
-              y: 0, // puts it at the bottom
-              w: 6,
-              h: 4,
-              static: false,
-              pageId: action.pageId,
-              isPage: action.isPage,
-            }),
-            newCounter: state.newCounter + 1,
-          });
-        } else {
-          message.warn("The reference has opened.");
-        }
+        return Object.assign({}, state, {
+          items: removeExisted.concat({
+            i: "n" + state.newCounter,
+            x: 6,
+            y: 0, // puts it at the bottom
+            w: 6,
+            h: 4,
+            static: false,
+            pageId: action.pageId,
+            isPage: action.isPage,
+          }),
+          newCounter: state.newCounter + 1,
+        });
       }
       return state;
 
