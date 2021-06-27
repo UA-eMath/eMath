3ac4d8b1c4b0bebd6fecec1296dd215d303000d5
xuechun <noglues666@gmail.com>
2021-06-27 18:43:29 -0400

    Update person model

--- backend/presentation/models.py
+++ backend/presentation/models.py
@@ -86,9 +86,9 @@ class Level(MPTTModel):
 
 
 class Person(models.Model):
-    first_name = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=20, blank=True)
     middle_name = models.CharField(max_length=20, null=True, blank=True)
-    last_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20, blank=True)
     type = models.CharField(max_length=20,
                             choices=ACCOUNT_TYPES,
                             default='Student')
