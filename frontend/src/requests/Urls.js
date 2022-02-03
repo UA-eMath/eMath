1f72b0142ffa8450c624cc4f943ff393e26d174c
Estelle Qiu <44105167+xqiu1@users.noreply.github.com>
2022-02-03 01:06:22 -0800

    Fix bug in Urls.js

--- frontend/src/requests/Urls.js
+++ frontend/src/requests/Urls.js
@@ -1,25 +1,6 @@
-const domain = {
-  local: "http://localhost",
-  http: "http://142.244.163.49",
-  https: "https://eMath.math.ualberta.ca",
-};
-
-function checkURL() {
-  if (
-    window.location.hostname === "localhost" ||
-    window.location.hostname === "127.0.0.1"
-  ) {
-    return domain["local"];
-  } else if (window.location.protocol === "https") {
-    return domain["https"];
-  } else {
-    return domain["http"];
-  }
-}
-
 export default {
-  domain: checkURL(),
-  port: window.location.protocol === "https" ? "" : 8000,
+  domain: window.location.origin,
+  port: window.location.protocol === "https:" ? "" : 8000,
   endpoints: {
     roots: {
       address: "database",
